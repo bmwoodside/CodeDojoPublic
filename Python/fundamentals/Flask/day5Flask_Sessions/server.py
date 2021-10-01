899357264dce1d37a79694f33f672987842fb6b9
Matt Woodside <bmwoodside@live.com>
2021-10-01 15:59:44 -0400

    day5 Sessions lecture updated

--- Python/fundamentals/Flask/day5Flask_Sessions/server.py
+++ Python/fundamentals/Flask/day5Flask_Sessions/server.py
@@ -6,21 +6,45 @@ app.secret_key="shhhh" # how do we obfuscate this?
 
 @app.route("/")
 def index():
+    if "voters" not in session:
+        session["voters"] = []
+
+    if "votes" not in session:
+        session["votes"] = {"total":0, "Toy Story":0, "The Incredibles":0, "Monsters Inc":0}
+
+
     return render_template("index.html")
 
 @app.route("/vote", methods=["POST"])
 def vote():
-    session["name"] = request.form["name"]
-    session["age"] = request.form["age"]
-    session["movie"] = request.form["movie"]
+    temp_user = {}
+    temp_user["name"] = request.form["name"]
+    temp_user["age"] = request.form["age"]
+    temp_user["movie"] = request.form["movie"]
+    
+    # voters = session["voters"]
+    # voters.append(temp_user)
+    # session["voters"] = voters
+
+    session["voters"].append(temp_user)
+
+    session["votes"]["total"] += 1
+    chosen_movie = request.form["movie"]
+    session["votes"][chosen_movie] += 1
+
+    session.modified = True
+
     return redirect("/results")
 
 
 @app.route("/results")
 def results():
-    return render_template("results.html", name=session["name"], age=session["age"], movie=session["movie"])
+    return render_template("results.html", voters=session["voters"])
 
-
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect("/")
 
 
 

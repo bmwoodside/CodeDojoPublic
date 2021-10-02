552d104eb62a01056ca04757d19b7bfeef05241d
Matt Woodside <bmwoodside@live.com>
2021-10-02 14:05:09 -0400

    wrapping up the Playgrounds Assignment

--- Python/fundamentals/Flask/PlaygroundAssignment/server.py
+++ Python/fundamentals/Flask/PlaygroundAssignment/server.py
@@ -10,10 +10,13 @@ def welcome():
 def play_home():
     return render_template("play.html")
 
-@app.route("/play/<int:num>")
-def play_numTimes(num):
-    return render_template("play2.html")
+@app.route("/play/<int:numInput>")
+def play_numTimes(numInput):
+    return render_template("play2.html", numberTimes = numInput)
 
+@app.route("/play/<int:numInput>/<string:color1>")
+def play_numTimes_changeColor(numInput, color1):
+    return render_template("play2.html", numberTimes = numInput, boxColor=color1)
 
 
 if __name__ == "__main__":

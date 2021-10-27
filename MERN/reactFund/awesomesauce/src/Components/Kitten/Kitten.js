19f85d8f3aec9660b83cb1675e1b371a73870f78
Matt Woodside <bmwoodside@live.com>
2021-10-27 15:16:05 -0400

    react demo additional commits

---
+++ MERN/reactFund/awesomesauce/src/Components/Kitten/Kitten.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Kitten extends Component {
+    render() {
+    return (
+        <img src="https://placekitten.com/300/300" alt="Kitten" />
+    );
+}
+}
+
+export default Kitten;

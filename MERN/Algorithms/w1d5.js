2d4a9eb1a39cfcc928770a79e4b59d8a311e76eb
Matt Woodside <bmwoodside@live.com>
2021-10-29 13:06:04 -0400

    updated algo for day5

--- MERN/Algorithms/w1d5.js
+++ MERN/Algorithms/w1d5.js
@@ -1,5 +1,22 @@
-let array1 = [490, 213, 720, 345, 367, 511]
+var array1 = [490, -213, 72, 345, 367, 511]
 
-// function radixSort(array){
-//     for (var i= 0; )
-// }
+function radix_sort(arr){
+    var inc = 1
+    for(var idx=0; idx < arr[0].toString().length; idx++){
+        for(var i = 1; i < arr.length; i++){
+            var temp = arr[i].toString()[(arr[i].toString().length)-inc];
+            temp == undefined? temp = 0: null;
+            console.log("temp: " +temp)
+            var tempFull = arr[i];
+            for(var j=i; j > 0 && temp < arr[j-1].toString()[arr[j-1].toString().length-inc]; j--){
+                arr[j] = arr[j-1];
+            }
+            arr[j] = tempFull;
+        }
+        inc++;
+        console.log(inc);
+    }
+    console.log(arr);
+}
+
+radix_sort(array1);

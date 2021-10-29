4487f05aa5fea0e1852e662d05bf2684fda8dc71
Matt Woodside <bmwoodside@live.com>
2021-10-28 21:14:06 -0400

    putting-it-together assignment completion

--- MERN/reactFund/putting-it-together/src/App.js
+++ MERN/reactFund/putting-it-together/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 
 import PersonCard from './Components/PersonCard'
 
-const person1 = {firstName: "Jane", lastName: "Doe", age: "45", hairColor: "Black"}
-const person2 = {firstName: "John", lastName: "Smith", age: "88", hairColor: "Brown"}
-const person3 = {firstName: "Millar", lastName: "Fillmore", age: "50", hairColor: "Brown"}
-const person4 = {firstName: "Maria", lastName: "Smith", age: "62", hairColor: "Brown"}
+const person1 = {firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Black"}
+const person2 = {firstName: "John", lastName: "Smith", age: 88, hairColor: "Brown"}
+const person3 = {firstName: "Millar", lastName: "Fillmore", age: 50, hairColor: "Brown"}
+const person4 = {firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Brown"}
 
 
 function App() {
   return (
     <div className="App">
-      <PersonCard person={person1}></PersonCard>
+      <PersonCard person={person1} />
       <PersonCard person={person2}></PersonCard>
       <PersonCard person={person3}></PersonCard>
       <PersonCard person={person4}></PersonCard>
--- MERN/reactFund/putting-it-together/src/Components/PersonCard.js
+++ MERN/reactFund/putting-it-together/src/Components/PersonCard.js
@@ -11,7 +11,10 @@ class PersonCard extends Component {
         }
     }
 
+    birthday = () => this.setState({ age: this.state.age += 1 })
+
     render() {
+        console.log(this.state.age)
         return (
             <div className="person">
                 <h1>
@@ -19,6 +22,9 @@ class PersonCard extends Component {
                 </h1>
                 <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.state.hairColor}</p>
+                <p>
+                    <input type="button" onClick={this.birthday} value="Button"></input>
+                </p>
             </div>
         )
     }

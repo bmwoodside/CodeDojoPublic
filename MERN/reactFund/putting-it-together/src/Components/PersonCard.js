767c141ecc8d7cfb4801a1d694656eb2b885e614
Matt Woodside <bmwoodside@live.com>
2021-10-28 20:31:20 -0400

    putting-it-together initial commit

---
+++ MERN/reactFund/putting-it-together/src/Components/PersonCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: this.props.person.firstName,
+            lastName: this.props.person.lastName,
+            age: this.props.person.age,
+            hairColor: this.props.person.hairColor
+        }
+    }
+
+    render() {
+        return (
+            <div className="person">
+                <h1>
+                    {this.state.lastName}, {this.state.firstName}
+                </h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.state.hairColor}</p>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;

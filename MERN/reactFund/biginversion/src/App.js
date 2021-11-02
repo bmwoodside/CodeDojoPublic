1c393d3078d33c02b3ebfcae1bcb278e1e0fdb91
Matt Woodside <bmwoodside@live.com>
2021-11-02 15:24:32 -0400

    bigInversion final commit

--- MERN/reactFund/biginversion/src/App.js
+++ MERN/reactFund/biginversion/src/App.js
@@ -6,12 +6,9 @@ const person2 = {firstName: "John", lastName: "Smith", age: 88, hairColor: "Brow
 const person3 = {firstName: "Millar", lastName: "Fillmore", age: 50, hairColor: "Brown"}
 const person4 = {firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Brown"}
 
-// console.log(person1)
-// console.log(PersonCard(person1))
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="test1" lastName="test2" age="test3" hairColor="test4"/>
       <PersonCard person={person1} />
       <PersonCard person={person2} />
       <PersonCard person={person3} />
--- MERN/reactFund/biginversion/src/Components/PersonCard.js
+++ MERN/reactFund/biginversion/src/Components/PersonCard.js
@@ -3,10 +3,9 @@ import React from 'react';
 const PersonCard = props => {
     return (
         <div>
-            <p>{props.lastName}</p>
-            <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
-            <p>Hair Color: { props.hairColor }</p>
+            <h1>{ props.person.lastName }, { props.person.firstName }</h1>
+            <p>Age: { props.person.age }</p>
+            <p>Hair Color: { props.person.hairColor }</p>
         </div>
     )
 }

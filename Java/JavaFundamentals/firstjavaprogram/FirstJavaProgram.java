7c56703c4d212ffdc80138bf74e59af84eedb7ea
Matt Woodside <bmwoodside@live.com>
2021-11-28 22:33:26 -0500

    java intro!

---
+++ Java/JavaFundamentals/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Matt");
+        System.out.println("I am 4,000 years old.");
+        System.out.println("My hometown is Houston, TX");
+    }
+}

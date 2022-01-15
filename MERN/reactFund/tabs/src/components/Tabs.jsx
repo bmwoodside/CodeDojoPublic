32c9b66a6bbddaca4e845c83acaa04f99f475efb
Matt Woodside <bmwoodside@live.com>
2022-01-15 01:43:48 -0500

    tabs updates

--- MERN/reactFund/tabs/src/components/Tabs.jsx
+++ MERN/reactFund/tabs/src/components/Tabs.jsx
@@ -10,21 +10,46 @@ const Tabs = props => {
         {header: "Tab 3", content: "Tab 3 Content"},
     ]);
 
-    const updateContent = (string) => {
-        console.log(string);
-        document.getElementById("contentContainer").innerText = string;
+    const [activeTab, setActiveTab] = useState([]);
+
+    const updateContent = (obj) => {
+        console.log(obj);
+        // setActiveTab(obj);
+        // document.getElementById("contentContainer").innerText = string;
+    }
+
+    let previousButton = null;
+
+    let testArrayFunction = (obj, btnIdx) => {
+        setActiveTab(obj[0]["content"]);
+        // reset previous button style to default
+        if (previousButton !== null) {
+            previousButton.style.backgroundColor = "";
+        } else if (previousButton === null) {
+            previousButton = btnIdx;
+        } else {
+            console.log("something broke in the testArrayFunction");
+        }
+        // set current button style to active
+        document.getElementById(btnIdx).style.backgroundColor = "yellow";
+        // document.getElementsByClassName("tabs-button").getElementById(btnIdx).style.backgroundColor = "blue";
+
+        // set current button to previous button (so we can reset it when the next tab is clicked again)
+        previousButton = btnIdx;
     }
 
     return (
+        <>
         <div className="tabs">
 
             <h1>Tabs:</h1>
             {tabs.map( (tab, i) => 
-                <div key={i}><button onClick={updateContent(tab.content)}>{tab.header}</button></div>
+                <button key={i} id={i} className="tabs-button" onClick={ (e) => testArrayFunction([tab, i]) }>{tab.header}</button>
             )}
-            <textarea id="contentContainer"></textarea>
-
         </div>
+        
+        <div id="contentContainer">{activeTab}</div>
+        </>
     )
 
 }

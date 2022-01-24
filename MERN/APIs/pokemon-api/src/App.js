05b7e9a84bb8503034dc6abeb63a9efa47fa56a8
Matt Woodside <bmwoodside@live.com>
2022-01-23 23:01:40 -0500

    rewrote GetPokemon file for 'axios pokemon api' assignement

--- MERN/APIs/pokemon-api/src/App.js
+++ MERN/APIs/pokemon-api/src/App.js
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* <GetPokemon /> */}
-      <FetchPokemon />
+      {/* <FetchPokemon /> */}
+      <GetPokemon />
     </div>
   );
 }
--- MERN/APIs/pokemon-api/src/Components/GetPokemon.jsx
+++ MERN/APIs/pokemon-api/src/Components/GetPokemon.jsx
@@ -1,39 +1,26 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 
-const GetPokemon = props => {
+const GetPokemon = (props) => {
 
     const [pokemon, setPokemon] = useState([]);
-    const [poke, setPoke] = useState([]);
-
-    useEffect(() => {
-            axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807&offset=807")
-                .then(response => {setPokemon(response.data.results)})
-                .catch(err => {console.log(err);})
-        }, [])
-
-        const renderPokemon = () => {
-            setPoke(pokemon)
-        }
 
     return (
         <div>
-            <button onClick={renderPokemon}>Fetch Pokemon</button>
+            <button onClick={() => {
+                axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+                    .then(response => { setPokemon(response.data.results) })
+            }}
+            >Fetch Pokemon</button>
+
             <ul>
-                {poke.map((pp) => {
-                    return <li>{pp.name}</li>
+                {pokemon.map( (onePokemon, i) => {
+                    return <li key={i}>{onePokemon.name}</li>
                 })}
             </ul>
         </div>
     )
 }
 
-
-
-
-
-
-
-
-export default GetPokemon
+export default GetPokemon;

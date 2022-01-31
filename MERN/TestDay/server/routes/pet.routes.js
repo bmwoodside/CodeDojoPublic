158b9ff7527649d45aa540fc8f4a382d475de570
Matt Woodside <bmwoodside@live.com>
2022-01-31 13:51:27 -0500

    updates

--- MERN/TestDay/server/routes/pet.routes.js
+++ MERN/TestDay/server/routes/pet.routes.js
@@ -4,7 +4,7 @@ const PetController = require('../controllers/pet.controller');
 module.exports = function(app) {
     // app.get('/', PetController.index);
     app.post('/api/pet', PetController.createPet);
-    app.get('/api/', PetController.getAllPets);
+    // app.get('/api/', PetController.getAllPets);
     app.get('/api/pets', PetController.getAllPets);
     app.get('/api/pets/:id', PetController.getPet);
     app.put('/api/pet/:id', PetController.updatePet);
